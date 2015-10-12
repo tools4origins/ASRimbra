f6ba013e1071c258f91a6bc5042e11e46f9673bc
atitalla <ismail.atitallah@telecom-sudparis.eu>
2015-10-12 12:28:46 +0200

    add DirectoryManager

--- DirectoryManager/src/main/java/edu.tsp.asr/DirectoryManager.java
+++ DirectoryManager/src/main/java/edu.tsp.asr/DirectoryManager.java
@@ -1,16 +1,21 @@
 package edu.tsp.asr;
 
+import edu.tsp.asr.entities.User;
+import edu.tsp.asr.exceptions.UserNotFoundException;
 import edu.tsp.asr.repositories.UserRepository;
 
+import java.util.List;
+
 import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class DirectoryManager {
-    public void add(User user);
-    public void remove(User user);
-    public void update(User user);
-    public User lookup(String name);
+   public static void main(String[] a) {
+        UserRepository userRepository = new UserMemoryRepository();
 
-    public static void main(String[] a) {
-
+       get("/addUser", (request, response) -> {
+           return "hellpoo";
+       });
 
     }
 }
