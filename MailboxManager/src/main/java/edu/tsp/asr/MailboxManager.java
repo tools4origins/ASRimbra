6421928d9ee673e533342fa16ae9263e07a6fbd6
Erwan Guyomarc'h <erwan.guyomarc_h@telecom-sudparis.eu>
2015-10-16 13:38:45 +0200

    use JSON for MailboxClient output

--- MailboxManager/src/main/java/edu/tsp/asr/MailboxManager.java
+++ MailboxManager/src/main/java/edu/tsp/asr/MailboxManager.java
@@ -6,6 +6,8 @@ import edu.tsp.asr.exceptions.MailNotFoundException;
 import edu.tsp.asr.exceptions.UserNotFoundException;
 import edu.tsp.asr.repositories.MailRepository;
 import edu.tsp.asr.repositories.UserRepository;
+import edu.tsp.asr.transformers.JsonTransformer;
+import spark.ResponseTransformer;
 
 import static spark.Spark.before;
 import static spark.Spark.get;
@@ -14,10 +16,14 @@ import static spark.Spark.post;
 
 public class MailboxManager {
     public static void main(String[] a) {
+        // config
         UserRepository userRepository = new UserMemoryRepository();
+        MailRepository mailRepository = new MailMemoryRepository();
+        ResponseTransformer transformer = new JsonTransformer();
+
+        // Populate repository
         userRepository.addUser(new User("guyomarc@tem-tsp.eu", "passwd"));
         userRepository.addUser(new User("atilalla@tem-tsp.eu", "passwd2"));
-        MailRepository mailRepository = new MailMemoryRepository();
         mailRepository.add(
                 new Mail(
                         "guyomarc@tem-tsp.eu",
@@ -35,7 +41,7 @@ public class MailboxManager {
                 )
         );
 
-        get("/", (request, response) -> userRepository.getAllUsers());
+        get("/", (request, response) -> userRepository.getAllUsers(), transformer);
 
         post("/connect/", (request, response) -> {
             try {
@@ -50,7 +56,7 @@ public class MailboxManager {
                 halt(403, "Bad credentials :(");
                 return "";
             }
-        });
+        }, transformer);
 
         before("/mailbox/*", (request, response) -> {
             if (request.session().attribute("user") == null) {
@@ -58,53 +64,54 @@ public class MailboxManager {
             }
         });
 
-        get("/mailbox/", ((request, response) -> {
+        get("/mailbox/", (request, response) -> {
             User user = request.session().attribute("user");
             return mailRepository.getMailByUser(user);
-        }));
+        }, transformer);
 
 
         get("/mailbox/disconnect/", (request, response) -> {
             request.session().removeAttribute("user");
             return "";
-        });
+        }, transformer);
 
-        get("/mailbox/:id", ((request, response) -> {
+        get("/mailbox/:id", (request, response) -> {
             User user = request.session().attribute("user");
 
             Integer id = 0;
             try {
                 id = Integer.parseInt(request.params(":id"));
             } catch (NumberFormatException e) {
-                response.status(400);
-                return "";
+                halt(400, "id is not a number");
             }
 
             try {
                 return mailRepository.getMailByUserAndId(user, id);
             } catch (MailNotFoundException e) {
-                response.status(404);
-                return "Mail not found";
+                halt(404, "Mail not found");
+                return null;
             }
-        }));
+        }, transformer);
 
-            post("/mailbox/send/", ((request, response) -> {
-                User user = request.session().attribute("user");
+        post("/mailbox/send/", (request, response) -> {
+            User user = request.session().attribute("user");
 
-                try {
-                    assert (request.queryParams("to") != null);
-                    assert (request.queryParams("title") != null);
-                    assert (request.queryParams("content") != null);
-                    String from = user.getMail();
-                    String to = request.queryParams("to");
-                    String title = request.queryParams("title");
-                    String content = request.queryParams("content");
-                    mailRepository.add(new Mail(from, to, title, content));
-                } catch (NullPointerException e) {
-                    halt(400, "Bad parameters");
-                }
-                return "";
-            }));
+            try {
+                assert (request.queryParams("to") != null);
+                assert (request.queryParams("title") != null);
+                assert (request.queryParams("content") != null);
+                String from = user.getMail();
+                String to = request.queryParams("to");
+                String title = request.queryParams("title");
+                String content = request.queryParams("content");
+                Mail mail = new Mail(from, to, title, content);
+                mailRepository.add(mail);
+                return mail.getId();
+            } catch (NullPointerException e) {
+                halt(400, "Bad parameters");
+                return null;
+            }
+        }, transformer);
 
         }
     }
